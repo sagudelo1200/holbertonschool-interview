10ba3662237d9fccd3d0d441a5a231a0485325f7
Santiago Agudelo <sagudelo1200@gmail.com>
2021-04-22 21:39:49 -0500

    Pycodestyle checked

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,12 +4,13 @@ sequentially from 0 to n - 1 and each box may contain keys to the other
 boxes.
 '''
 
+
 def canUnlockAll(boxes):
     '''method that determines if all the boxes can be opened.
     '''
     for key in range(1, len(boxes[1::])):
         res = False
-        
+
         for idx in range(len(boxes)):
             res = (key in boxes[idx] and key != idx)
             if res:
